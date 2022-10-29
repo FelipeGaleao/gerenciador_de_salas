229b6f72b47c550e839abf510af022c2043fe1af
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-10-29 16:18:20 -0300

    fix: comentado os endpoints para as entidades echo e dummy

--- backend/localiza_sala_backend/web/api/router.py
+++ backend/localiza_sala_backend/web/api/router.py
@@ -5,6 +5,6 @@ from localiza_sala_backend.web.api import docs, dummy, echo, monitoring, users
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
+# api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
+# api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
