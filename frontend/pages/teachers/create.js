75ec55e5851cbc3d464bc6b64ed451a672a6ddf5
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-11-05 01:48:18 -0300

    feat: adicionado página para adicionar um novo professor

---
+++ frontend/pages/teachers/create.js
@@ -0,0 +1,131 @@
+import * as Yup from 'yup';
+import { useForm, yupResolver } from '@mantine/form';
+import { NumberInput, Textarea, TextInput, Button, Box, Group, Paper, Title, Text, LoadingOverlay, Notification, Checkbox, Container } from '@mantine/core';
+import { Grid } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { showNotification } from '@mantine/notifications';
+import Router from 'next/router';
+import { useSelector, useDispatch } from "react-redux";
+import React from 'react';
+import App from 'next/app';
+
+export default function RoomsIndexPage() {
+    const user_logado = useSelector((state) => state.user);
+    const [visible, setVisible] = useState(false);
+    let [rooms, setRooms] = useState([]);
+
+    const schema = Yup.object().shape({
+        nome: Yup.string().min(2, 'Seu nome deve ter pelo menos 2 caracteres').required('Nome do professor é obrigatório'),
+        sobrenome: Yup.string().min(2, 'Seu sobrenome deve ter pelo menos 2 caracteres').required('Sobrenome do professor é obrigatório'),
+        siafi: Yup.number().min(1, 'O siafi deve ser maior que 0').required('O siafi é obrigatório'),
+        lotacao: Yup.string().min(2, 'A lotação deve ter pelo menos 2 caracteres').required('A lotação é obrigatória'),
+    });
+
+    const form = useForm({
+        validate: yupResolver(schema),
+        initialValues: {
+        },
+    });
+
+    const handleSubmit = async (values) => {
+        setVisible((v) => !v);
+
+        const room_to_create = {
+            'nome': values.nome,
+            'sobrenome': values.sobrenome,
+            'siafi': values.siafi,
+            'lotacao': values.lotacao,
+        };
+
+        const response = await fetch('http://localhost:8000/api/teachers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + user_logado.access_token
+            },
+            body: JSON.stringify(room_to_create)
+        });
+        const data = await response.json();
+        setVisible((v) => !v);
+        if (data.error) {
+            showNotification({
+                title: 'Erro ao criar professor!',
+                message: data.error,
+                color: 'red',
+                position: 'br',
+            });
+        } else {
+            showNotification({
+                title: 'Professor criado com sucesso!',
+                message: data.message + ':)',
+                color: 'teal',
+                position: 'br',
+            });
+            Router.push('/teachers');
+        }
+    }
+
+    return (
+        <Container size="xl">
+            <Grid>
+                <Grid.Col md={12}>
+                    <Paper shadow="lg" radius="md" p="xl">
+                        <Grid grow>
+                            <Grid.Col md={10}>
+                                <Title>Professores</Title>
+                                <Title order={4}>Adicionando um novo professor</Title>
+                            </Grid.Col>
+                            <hr></hr>
+                        </Grid>
+                        <Grid.Col md={12}>
+                            <hr style={{ color: "#495057", marginBottom: "30px", opacity: "0.2" }} />
+                            <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+
+                                <Grid>
+                                    <LoadingOverlay visible={visible} overlayBlur={1.5} />
+
+                                    <Grid.Col md={6} sm={12}> <TextInput
+                                        withAsterisk
+                                        label="Nome"
+                                        placeholder="Ex.:  Francisco Morato"
+                                        {...form.getInputProps('nome')}
+                                    />
+                                    </Grid.Col>
+                                    <Grid.Col md={6} sm={12}> <TextInput
+                                        withAsterisk
+                                        label="Sobrenome"
+                                        placeholder="Ex.:  Galvão"
+                                        {...form.getInputProps('sobrenome')}
+                                    />
+                                    </Grid.Col>
+                                    <Grid.Col md={6} sm={12}> <TextInput
+                                        withAsterisk
+                                        label="Lotação"
+                                        placeholder="Ex.: Faculdade de Computacao / UFMS"
+                                        {...form.getInputProps('lotacao')}
+                                    />
+                                    </Grid.Col>
+                                    <Grid.Col md={6} sm={12}> <TextInput
+                                        withAsterisk
+                                        type={"number"}
+                                        label="SIAFI"
+                                        placeholder="Ex.: 012345"
+                                        {...form.getInputProps('siafi')}
+                                    />
+                                    </Grid.Col>
+                    
+
+                                </Grid>
+
+                                <Group position="right" mt="xl">
+                                    <Button onClick={(e) => Router.push('/rooms/')} variant="outline" color="gray" style={{ marginRight: "10px" }}>Voltar</Button>
+                                    <Button type="submit">Cadastrar</Button>
+                                </Group>
+                            </form>
+                        </Grid.Col>
+                    </Paper>
+                </Grid.Col>
+            </Grid>
+        </Container>
+    );
+}
---
+++ frontend/pages/teachers/index.js
@@ -0,0 +1,73 @@
+import * as Yup from 'yup';
+import { useForm, yupResolver } from '@mantine/form';
+import { NumberInput, TextInput, Button, Box, Group, Paper, Title, Text, LoadingOverlay, Notification, Container } from '@mantine/core';
+import { Grid } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { showNotification } from '@mantine/notifications';
+import Router from 'next/router';
+import { useSelector, useDispatch } from "react-redux";
+import React from 'react';
+import App from 'next/app';
+import axios from 'axios';
+import { TableSort } from '../teachers/components/table';
+
+export default function RoomsIndexPage() {
+  const user_logado = useSelector((state) => state.user);
+  let [teachers, setTeachers] = useState([]);
+  const getTeachers = async () => {
+    try {
+      await axios.get('http://localhost:8000/api/teachers', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + user_logado.access_token
+        }
+      })
+        .then(response => {
+          setTeachers(response.data)
+          console.log(response.data)
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
+  useEffect(() => {
+    getTeachers();
+  }, []);
+
+
+  return (
+    <Container size="xl">
+      <Grid>
+        <Grid.Col md={12}>
+          <Paper shadow="lg" radius="md" style={{ "padding": "1.5%" }}>
+            <Grid grow>
+              <Grid.Col md={9} style={{ "paddingLeft": "30px" }}>
+                <Title style={{ "fontWeight": "600" }}>Professores</Title>
+                <Title order={4}>Lista de professores cadastrados</Title>
+              </Grid.Col>
+              <Grid.Col md={3}>
+                <Button color="blue" onClick={(e) => Router.push('/teachers/create')}>
+                  Cadastrar um novo professor
+                </Button>
+              </Grid.Col>
+            </Grid>
+            <Grid.Col md={12}>
+              {teachers.length > 0 ? (
+                <TableSort data={teachers} />
+              ) : (
+                <Text>Carregando...</Text>
+              )}
+
+              {/*                 
+                  <TableSort data={data} /> */}
+            </Grid.Col>
+          </Paper>
+        </Grid.Col>
+      </Grid>
+    </Container>
+  );
+}
