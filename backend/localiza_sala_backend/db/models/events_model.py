9e94a273b325d458a5e576f589209254305c3c5b
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-11-13 18:01:37 -0300

    fix: adicionado modificações na entidade evento

--- backend/localiza_sala_backend/db/models/events_model.py
+++ backend/localiza_sala_backend/db/models/events_model.py
@@ -1,26 +1,9 @@
 from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import DateTime, Integer, String
+from sqlalchemy.sql.sqltypes import DateTime, Integer, String, Time
 from localiza_sala_backend.db.base import Base
 from sqlalchemy import ForeignKey
 
 
-# Table events {
-#   id int
-#   nome str
-#   descricao str
-#   quantidade_de_pessoas int
-#   nome_curso str
-#   nome_faculdade str
-#   periodo str
-#   dt_inicio_evento datetime
-#   dt_fim_evento datetime
-#   hr_inicio_evento datetime
-#   hr_fim_evento datetime
-#   criado_por int [ref: > users.id]
-#   atualizado_por int [ref: > users.id]
-#   dt_criacao datetime
-#   dt_modificacao datetime
-# }
 
 class EventsModel(Base):
     """Model para Eventos. """
@@ -35,8 +18,8 @@ class EventsModel(Base):
     nome_faculdade = Column(String(length=200))  # noqa: WPS432
     dt_inicio_evento = Column(DateTime())
     dt_fim_evento = Column(DateTime())
-    hr_inicio_evento = Column(DateTime())
-    hr_fim_evento = Column(DateTime())
+    hr_inicio_evento = Column(Time(timezone=False))
+    hr_fim_evento =  Column(Time(timezone=False))
     criado_por = Column(Integer(), ForeignKey("users.id"))
     atualizado_por = Column(Integer(), ForeignKey("users.id"))
     dt_criacao = Column(DateTime())
