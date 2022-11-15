8b5b06cd91ac2d28f01847a6aff22b4bf7f8657c
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-11-15 13:54:47 -0300

    feat: adicionado model, rotas, e schema para entidade de reservas

--- backend/localiza_sala_backend/db/models/reservations.py
+++ backend/localiza_sala_backend/db/models/reservations.py
@@ -1,5 +1,5 @@
 from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import DateTime, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import DateTime, Integer, String, Boolean, Time
 from sqlalchemy.orm import relationship
 from localiza_sala_backend.db.base import Base
 from sqlalchemy import ForeignKey
